3e980d7add566f64c8f8155171be7bfa6abfcee8
braian <braianve7@gmail.com>
2024-10-03 10:20:43 -0300

    empezamos con la venta de criptomonedas

--- src/views/SaleView.vue
+++ src/views/SaleView.vue
@@ -1,6 +1,28 @@
 <template>
-  <h1>aca se vende</h1>
+  <div class="container">
+    <div>
+      <h2>Vender Criptomoneda</h2>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
+export default{
+  computed:{
+    /*...mapGetters('transaccion', ['userHistory']),
+    ...mapGetters(['username']),*/
+  },
+  methods:{
+    //...mapActions('transaccion', ['dataHistory']),
+  },
+}
+
 </script>
+
+<style scoped>
+.container{
+  background-color: rgb(91, 103, 91);
+}
+</style>
